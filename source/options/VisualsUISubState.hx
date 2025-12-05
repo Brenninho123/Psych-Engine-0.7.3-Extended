f14f3414dc1b0f6d5a741d6102a5834c92bf034c
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2025-12-05 14:52:58 -0300

    Atualizar o VisualsUISubState.hx

--- source/options/VisualsUISubState.hx
+++ source/options/VisualsUISubState.hx
@@ -137,14 +137,6 @@ class VisualsUISubState extends BaseOptionsMenu
 			pauseMusics);
 		addOption(option);
 		option.onChange = onChangePauseMusic;
-		
-		#if CHECK_FOR_UPDATES
-		var option:Option = new Option('Check for Updates',
-			'On Release builds, turn this on to check for updates when you start the game.',
-			'checkForUpdates',
-			'bool');
-		addOption(option);
-		#end
 
 		#if DISCORD_ALLOWED
 		var option:Option = new Option('Discord Rich Presence',
